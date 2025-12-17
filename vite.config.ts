658f1ca202d326f1e9ea07639ec167844c0cceaf
samufelipe <samucafe01@gmail.com>
2025-12-17 15:16:52 -0300

    Ajuste para Deployment do app na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  // Carrega as variáveis do arquivo .env ou do painel da Vercel
-  const env = loadEnv(mode, '.', '');
+  const env = loadEnv(mode, process.cwd(), '');
   
   return {
     plugins: [react()],
     define: {
-      // Esta é a forma mais segura de injetar as chaves para o navegador
       'process.env.API_KEY': JSON.stringify(env.API_KEY || env.VITE_API_KEY),
       'process.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
       'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY),
     },
+    server: {
+      port: 3000,
+    },
     build: {
       outDir: 'dist',
       sourcemap: false,
-      emptyOutDir: true
+      emptyOutDir: true,
     }
   };
 });
