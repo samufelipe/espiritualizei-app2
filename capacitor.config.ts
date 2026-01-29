57321a0d22df0d055d7294bf17fd227e5ef295c0
samufelipe <209116608+samufelipe@users.noreply.github.com>
2026-01-28 21:01:09 -0500

    feat: native splash screen configuration and assets structure

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,30 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.espiritualizei.app',
   appName: 'Espiritualizei',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    Keyboard: {
+      resize: 'body',
+      style: 'dark',
+      resizeOnFullScreen: true
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#1A2530",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      androidSpinnerStyle: "large",
+      iosSpinnerStyle: "small",
+      spinnerColor: "#A78BFA",
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
